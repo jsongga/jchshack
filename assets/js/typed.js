0933775537cbdbd17ca834deccc3e3449db48b0d
John Song <johnsong14306@gmail.com>
2021-09-12 18:59:12 -0400

    Added assets

---
+++ assets/js/typed.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#typedhack", {
+    strings: [
+    'JCHS Programming Club',
+    'JCHS Hack Club'
+    ],
+    backSpeed: 50,
+    backDelay: 2000,
+    typeSpeed: 60,
+    smartBackspace: true
+
+  });
+    
